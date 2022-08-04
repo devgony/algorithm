11ab6c1ab7dad3bd1e861ce3449bdd6a5dd4b487
devgony <devgony@gmail.com>
2022-08-04 13:20:19 +0000

    feat: NDB/92.big-number-rule

---
+++ NDB/92.big-number-rule.py
@@ -0,0 +1,20 @@
+# n, m, k = map(int, input().split())
+n, m, k = [5, 8, 3]
+# numbers = list(map(int, input().split()))
+numbers = [2, 4, 5, 4, 6]
+first, second, *rest = sorted(numbers, reverse=True)
+result = 0
+while m > 0:
+    if m > k:
+        result += first * k + second
+        m -= k + 1
+    else:
+        result += first * m
+        m = 0
+
+# Answer
+# num_first = (m // (k+1)) * k + m % (k+1)
+# result += num_first * first
+# result += (m - num_first) * second
+
+print(result)
